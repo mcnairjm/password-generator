f6d1996d3169817fb2277519b80872b11eaadc44
mcnairjm19 <mcnairjm1993@gmail.com>
2021-12-04 10:22:16 -0500

    added arrays

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 // Assignment code here
 var passwordLength = '';
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numberChars = '1234567890'
-var symbolChars = '!@#$%^&*()-=+?/><';
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var symbolChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+', '?', '/', '>', '<'];
 
 function generatePassword() {
   prompts();
